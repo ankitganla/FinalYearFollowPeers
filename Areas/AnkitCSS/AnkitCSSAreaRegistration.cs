c79ec7a96d1113c094e0c11926aba30ee6af84a1
ankitganla <ankitganla@gmail.com>
2014-02-21 18:04:37 +0800

    all files included

---
+++ Areas/AnkitCSS/AnkitCSSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FollowPeers.Areas.AnkitCSS
+{
+    public class AnkitCSSAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "AnkitCSS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "AnkitCSS_default",
+                "AnkitCSS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
